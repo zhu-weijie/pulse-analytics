7add5e9d5e96d884598bd0ad9d014682307565b3
zhuwj <byzhubaiyuan@gmail.com>
2025-08-26 16:23:44 +0800

    Fix host_data_path

--- src/pulse_analytics/jobs/trigger.py
+++ src/pulse_analytics/jobs/trigger.py
@@ -1,3 +1,5 @@
+import os
+
 import docker
 from docker.errors import DockerException
 from fastapi import HTTPException
@@ -9,14 +11,25 @@ def run_spark_job_container() -> dict:
         image_name = "pulse-analytics-spark-job:latest"
         network_name = "pulse-analytics_default"
 
+        host_project_path = os.getenv("HOST_PROJECT_PATH")
+        if not host_project_path:
+            raise ValueError("HOST_PROJECT_PATH environment variable not set.")
+
+        host_data_path = os.path.join(host_project_path, "data")
+        container_data_path = "/app_data"
+
         print(f"Attempting to run container from image: {image_name}")
+        print(
+            f"Mounting host path '{host_data_path}' to container "
+            f"path '{container_data_path}'"
+        )
 
         container = client.containers.run(
             image=image_name,
             detach=True,
             network=network_name,
             auto_remove=True,
-            volumes={"/app_data": {"bind": "/app_data", "mode": "ro"}},
+            volumes={host_data_path: {"bind": container_data_path, "mode": "ro"}},
         )
 
         print(f"Successfully started container: {container.id}")
