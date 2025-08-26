8f2e99f0b6dd0b394355841c7ba7a7aeb81299c5
zhuwj <byzhubaiyuan@gmail.com>
2025-08-26 10:42:54 +0800

    Refactor the Transformation Logic

--- src/pulse_analytics/transforms/transformations.py
+++ src/pulse_analytics/transforms/transformations.py
@@ -1,15 +1,11 @@
 from pyspark.sql import DataFrame
-from pyspark.sql.functions import col, to_date
+from pyspark.sql.functions import to_date
 
 
 def transform_orders(source_df: DataFrame) -> DataFrame:
-    transformed_df = source_df
-
-    transformed_df = transformed_df.withColumnRenamed("order_value", "revenue_usd")
-
-    transformed_df = transformed_df.withColumn(
-        "order_date", to_date(col("order_date"), "yyyy-MM-dd")
-    )
+    transformed_df = source_df.withColumnRenamed(
+        "order_value", "revenue_usd"
+    ).withColumn("order_date", to_date("order_date", "yyyy-MM-dd"))
 
     print("PySpark transformation successful.")
     return transformed_df
